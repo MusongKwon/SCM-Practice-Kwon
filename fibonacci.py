9a8cf4b3816f3e199636a4419d407bedf58be1d8
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-03-28 12:40:20 -0500

    Make fibonacci recursive

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,13 @@
 def fibonacci(n):
-    seq=[0,1]
-    for i in range(2, n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        seq = fibonacci(n - 1)
         seq.append(seq[-1] + seq[-2])
-    return seq[:n]
+        return seq
 
 print(fibonacci(10))
